miyu-xu/system-core

storageproxyd: Add checkpoint state interface
Trusty Storage TA needs to be able to check whether storage is
checkpointing or not so that it knows when to write a forward checkpoint
instead of to the super block.

Bug: 393406566
Test: m && ./vendor/google/tools/flashall && adb wait-for-device && adb logcat -s storageproxyd:v vold:V Checkpoint:V
Change-Id: Ic5f618cf22a82b9c0c7683c67495999f8a96d0cd

trusty/storage/proxy/checkpoint_handling.cpp
@@ -15,6 +15,7 @@
  */
 
 #include "checkpoint_handling.h"
+#include "ipc.h"
 #include "log.h"
 
 #include <fstab/fstab.h>
@@ -22,11 +23,36 @@
 #include <cstring>
 #include <string>
 
+#include <aidl/android/system/vold/BnVoldCheckpointListener.h>
+#include <aidl/android/system/vold/IVold.h>
+#include <android/binder_auto_utils.h>
+#include <android/binder_interface_utils.h>
+#include <android/binder_manager.h>
 #include <libgsi/libgsi.h>
 
 namespace {
 
+using ::aidl::android::system::vold::BnVoldCheckpointListener;
+using ::aidl::android::system::vold::CheckpointingState;
+using ::aidl::android::system::vold::IVold;
+using ::aidl::android::system::vold::toString;
+using ::ndk::ScopedAStatus;
+using ::ndk::SharedRefBase;
+
+const char kVoldService[] = "android.system.vold.IVold/default";
+
 bool checkpointingDoneForever = false;
+
+std::atomic<bool> voldPossibleCheckpointing = true;
+
+class VoldListener : public BnVoldCheckpointListener {
+  public:
+    ScopedAStatus onCheckpointingComplete() final {
+        voldPossibleCheckpointing.store(false);
+        ALOGD("VoldCheckpointListener reported checkpointing complete\n");
+        return ScopedAStatus::ok();
+    }
+};
 
 }  // namespace
 
@@ -89,4 +115,57 @@ int is_data_checkpoint_active(bool* active) {
 bool is_gsi_running() {
     /* TODO(b/210501710): Expose GSI image running state to vendor storageproxyd */
     return !access(android::gsi::kGsiBootedIndicatorFile, F_OK);
+}
+
+int vold_connect() {
+    auto binder = ndk::SpAIBinder(AServiceManager_waitForService(kVoldService));
+    if (binder == nullptr) {
+        ALOGD("Got null binder for %s (was %sdeclared)\n", kVoldService,
+              AServiceManager_isDeclared(kVoldService) ? "" : "not ");
+        return -1;
+    }
+
+    auto vold = IVold::fromBinder(binder);
+    if (vold == nullptr) {
+        ALOGI("Could not convert binder to android::system::vold::IVold\n");
+        return -1;
+    }
+
+    CheckpointingState state;
+    ScopedAStatus ret =
+            vold->registerCheckpointListener(SharedRefBase::make<VoldListener>(), &state);
+    if (!ret.isOk()) {
+        ALOGE("Could not register VoldCheckpointListener: %s\n", ret.getDescription().c_str());
+        return -1;
+    }
+    ALOGI("Registered VoldCheckpointListener in %s\n", toString(state).c_str());
+
+    if (state == CheckpointingState::CHECKPOINTING_COMPLETE) {
+        voldPossibleCheckpointing.store(false);
+    }
+    return 0;
+}
+
+static storage_checkpoint_state checkpointing_state() {
+    if (voldPossibleCheckpointing.load()) {
+        return STORAGE_CHECKPOINT_STATE_POSSIBLE;
+    }
+    return STORAGE_CHECKPOINT_STATE_DONE;
+}
+
+int checkpointing_get_state(struct storage_msg* msg, const void*, size_t req_len, struct watcher*) {
+    if (req_len != 0) {
+        ALOGE("%s: invalid request length (%zu != %d)\n", __func__, req_len, 0);
+        msg->result = STORAGE_ERR_NOT_VALID;
+        goto err_response;
+    }
+
+    msg->result = STORAGE_NO_ERROR;
+    struct storage_checkpointing_state_resp resp;
+    resp.data = checkpointing_state();
+    ALOGD("Read checkpointing state %d\n", resp.data);
+    return ipc_respond(msg, &resp, sizeof(resp));
+
+err_response:
+    return ipc_respond(msg, NULL, 0);
 }

Mohithash/system_core

trusty/storage/proxy/checkpoint_handling.cpp
@@ -15,6 +15,7 @@
  */
 
 #include "checkpoint_handling.h"
+#include "ipc.h"
 #include "log.h"
 
 #include <fstab/fstab.h>
@@ -22,11 +23,39 @@
 #include <cstring>
 #include <string>
 
+#include <aidl/android/system/vold/BnVoldCheckpointListener.h>
+#include <aidl/android/system/vold/IVold.h>
+#include <android/binder_auto_utils.h>
+#include <android/binder_interface_utils.h>
+#include <android/binder_manager.h>
 #include <libgsi/libgsi.h>
 
 namespace {
 
+using ::aidl::android::system::vold::BnVoldCheckpointListener;
+using ::aidl::android::system::vold::CheckpointingState;
+using ::aidl::android::system::vold::IVold;
+using ::aidl::android::system::vold::toString;
+using ::ndk::ScopedAStatus;
+using ::ndk::SharedRefBase;
+
+const char kVoldService[] = "android.system.vold.IVold/default";
+
 bool checkpointingDoneForever = false;
+
+/* Written once from main thread before any checkpointing_get_state
+ * calls could be reading it. */
+bool voldConnected = false;
+std::atomic<bool> voldPossibleCheckpointing = true;
+
+class VoldListener : public BnVoldCheckpointListener {
+  public:
+    ScopedAStatus onCheckpointingComplete() final {
+        voldPossibleCheckpointing.store(false);
+        ALOGD("VoldCheckpointListener reported checkpointing complete\n");
+        return ScopedAStatus::ok();
+    }
+};
 
 }  // namespace
 
@@ -89,4 +118,65 @@ int is_data_checkpoint_active(bool* active) {
 bool is_gsi_running() {
     /* TODO(b/210501710): Expose GSI image running state to vendor storageproxyd */
     return !access(android::gsi::kGsiBootedIndicatorFile, F_OK);
+}
+
+int vold_connect() {
+    auto binder = ndk::SpAIBinder(AServiceManager_waitForService(kVoldService));
+    if (binder == nullptr) {
+        ALOGE("Got null binder for %s (was %sdeclared)\n", kVoldService,
+              AServiceManager_isDeclared(kVoldService) ? "" : "not ");
+        return 0;
+    }
+
+    auto vold = IVold::fromBinder(binder);
+    if (vold == nullptr) {
+        ALOGE("Could not convert binder to android::system::vold::IVold\n");
+        return 0;
+    }
+
+    CheckpointingState state;
+    ScopedAStatus ret =
+            vold->registerCheckpointListener(SharedRefBase::make<VoldListener>(), &state);
+    if (!ret.isOk()) {
+        ALOGE("Could not register VoldCheckpointListener: %s\n", ret.getDescription().c_str());
+        return 0;
+    }
+    ALOGD("Registered VoldCheckpointListener in %s\n", toString(state).c_str());
+
+    if (state == CheckpointingState::CHECKPOINTING_COMPLETE) {
+        voldPossibleCheckpointing.store(false);
+    }
+
+    voldConnected = true;
+    return 0;
+}
+
+int checkpointing_get_state(struct storage_msg* msg, const void*, size_t req_len, struct watcher*) {
+    if (req_len != 0) {
+        ALOGE("%s: invalid request length (%zu != %d)\n", __func__, req_len, 0);
+        msg->result = STORAGE_ERR_NOT_VALID;
+        goto err_response;
+    }
+
+    if (!voldConnected) {
+        ALOGE("Checkpointing state requested but no connection to vold.\n");
+        msg->result = STORAGE_ERR_UNIMPLEMENTED;
+        goto err_response;
+    }
+
+    msg->result = STORAGE_NO_ERROR;
+    struct storage_checkpointing_state_resp resp;
+    resp.state = voldPossibleCheckpointing.load() ? STORAGE_CHECKPOINT_STATE_POSSIBLE
+                                                  : STORAGE_CHECKPOINT_STATE_DONE;
+
+    /*
+     * TODO: Get the real boot slot so that when storage boots with an on-disk checkpoint, it can
+     * tell whether it should be used or discarded.
+     */
+    resp.boot_slot = -1;
+    ALOGD("Read checkpointing state %d\n", resp.state);
+    return ipc_respond(msg, &resp, sizeof(resp));
+
+err_response:
+    return ipc_respond(msg, NULL, 0);
 }