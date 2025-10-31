Mohithash/system_core

commit 4397f3730be80e00b86966c571f9368c4626e39f

Refactor USB controller setup to be more robust
Add a new property -- ro.boot.wait_for_udc -- that can be used in
recovery and charger mode to wait for the sys.usb.controller to be
detected before triggering `late-init`. This ensures that fastbootd,
recovery, and charger services don't start before the USB controller is
available which can lead to the USB gadget not being configured
correctly.

If the USB controller never comes up, then we will timeout after 10
seconds to allow init to continue. Init will continue to check if the
USB controller is available in the same way it currently does.

Test: reboot to fastbootd 100x to verify USB detection
Test: reboot to recovery 100x to verify USB detection
Test: reboot to charger mode to verify USB detection
Test: normal reboots x100 and verify adb
Bug: 448889416
Change-Id: I50143ffb7510a6e6a2b3406b1802be83728f8252

init/init.cpp
@@ -31,6 +31,7 @@
 #include <sys/utsname.h>
 #include <unistd.h>
 
+#include <condition_variable>
 #include <filesystem>
 #include <fstream>
 #include <functional>
@@ -133,6 +134,11 @@ struct PendingControlMessage {
 };
 [[clang::no_destroy]] static std::mutex pending_control_messages_lock;
 [[clang::no_destroy]] static std::queue<PendingControlMessage> pending_control_messages;
+
+[[clang::no_destroy]] static std::condition_variable udc_detection_cv;
+[[clang::no_destroy]] static std::mutex udc_controller_lock;
+static auto udc_controller_set = false;
+static auto udc_timeout = false;
 
 // Init epolls various FDs to wait for various inputs.  It previously waited on property changes
 // with a blocking socket that contained the information related to the change, however, it was easy
@@ -679,23 +685,50 @@ static Result<void> queue_property_triggers_action(const BuiltinArguments& args)
     return {};
 }
 
+// Needs to be called after PropertyInit() is called
+static BootMode GetBootMode() {
+    auto bootmode = GetProperty("ro.bootmode", GetProperty("ro.boot.mode", ""));
+    if (bootmode == "charger") {
+        return BootMode::CHARGER_MODE;
+    } else if (IsRecoveryMode() && GetIntProperty("ro.boot.force_normal_boot", 0) == 0) {
+        return BootMode::RECOVERY_MODE;
+    }
+
+    return BootMode::NORMAL_MODE;
+}
+
 // Set the UDC controller for the ConfigFS USB Gadgets.
 // Read the UDC controller in use from "/sys/class/udc".
 // In case of multiple UDC controllers select the first one.
-static void SetUsbController() {
-    static auto controller_set = false;
-    if (controller_set) return;
-    std::unique_ptr<DIR, decltype(&closedir)> dir(opendir("/sys/class/udc"), closedir);
-    if (!dir) return;
+static void SetUsbController(bool run_loop) {
+    if (udc_controller_set) return;
 
-    dirent* dp;
-    while ((dp = readdir(dir.get())) != nullptr) {
-        if (dp->d_name[0] == '.') continue;
+    do {
+        std::unique_ptr<DIR, decltype(&closedir)> dir(opendir("/sys/class/udc"), closedir);
+        if (!dir) return;
 
-        SetProperty("sys.usb.controller", dp->d_name);
-        controller_set = true;
-        break;
-    }
+        dirent* dp;
+        while ((dp = readdir(dir.get())) != nullptr) {
+            if (dp->d_name[0] == '.') continue;
+
+            SetProperty("sys.usb.controller", dp->d_name);
+            std::lock_guard<std::mutex> lock(udc_controller_lock);
+            udc_controller_set = true;
+
+            // Recovery and Charger mode always uses configfs=1.
+            if (GetBootMode() != BootMode::NORMAL_MODE) {
+                SetProperty("sys.usb.configfs", "1");
+            }
+            run_loop = false;
+            break;
+        }
+
+        if (run_loop && !udc_timeout) {
+            std::this_thread::sleep_for(100ms);
+        }
+    } while (!udc_timeout && run_loop);
+
+    udc_detection_cv.notify_one();
 }
 
 /// Set ro.kernel.version property to contain the major.minor pair as returned
@@ -1167,7 +1200,20 @@ int SecondStageMain(int argc, char** argv) {
     fs_mgr_vendor_overlay_mount_all();
     export_oem_lock_status();
     MountHandler mount_handler(&epoll);
-    SetUsbController();
+
+    BootMode bootmode = GetBootMode();
+
+    // Set the default value of sys.usb.configfs
+    SetProperty("sys.usb.configfs", "0");
+
+    std::unique_ptr<std::thread> usb_controller_thread;
+    bool wait_for_udc =
+            bootmode != BootMode::NORMAL_MODE && GetBoolProperty("ro.boot.wait_for_udc", false);
+    if (!wait_for_udc) {
+        SetUsbController(false);
+    } else {
+        usb_controller_thread = std::make_unique<std::thread>(&SetUsbController, true);
+    }
     SetKernelVersion();
 
     const BuiltinFunctionMap& function_map = GetBuiltinFunctionMap();
@@ -1237,9 +1283,21 @@ int SecondStageMain(int argc, char** argv) {
 
     // Copy logs captures from pstore. Flush the logs when boot completes
     am.QueueBuiltinAction(SetCopyRollbackLogsAction, "CopyRollbackLogs");
+
+    if (wait_for_udc) {
+        std::unique_lock<std::mutex> udc_lock(udc_controller_lock);
+        bool ret = udc_detection_cv.wait_for(udc_lock, std::chrono::seconds(10),
+                                             [] { return udc_controller_set; });
+        if (!ret) {
+            // Notify the SetUsbController thread to exit
+            udc_timeout = true;
+        }
+        udc_lock.unlock();
+        usb_controller_thread->join();
+    }
+
     // Don't mount filesystems or start core system services in charger mode.
-    std::string bootmode = GetProperty("ro.bootmode", "");
-    if (bootmode == "charger") {
+    if (bootmode == BootMode::CHARGER_MODE) {
         am.QueueEventTrigger("charger");
     } else {
         am.QueueEventTrigger("late-init");
@@ -1298,7 +1356,7 @@ int SecondStageMain(int argc, char** argv) {
         }
         if (!IsShuttingDown()) {
             HandleControlMessages();
-            SetUsbController();
+            SetUsbController(false);
         }
     }
 

init/first_stage_init.cpp
@@ -63,12 +63,6 @@ namespace init {
 
 namespace {
 
-enum class BootMode {
-    NORMAL_MODE,
-    RECOVERY_MODE,
-    CHARGER_MODE,
-};
-
 void FreeRamdisk(DIR* dir, dev_t dev) {
     int dfd = dirfd(dir);
 

init/util.h
@@ -40,6 +40,12 @@ enum mount_mode {
     MOUNT_MODE_LATE = 2,
 };
 
+enum class BootMode {
+    NORMAL_MODE,
+    RECOVERY_MODE,
+    CHARGER_MODE,
+};
+
 static const char kColdBootDoneProp[] = "ro.cold_boot_done";
 
 extern void (*trigger_shutdown)(const std::string& command);

rootdir/init.usb.rc
@@ -23,11 +23,6 @@ service adbd /system/bin/adbd --root_seclabel=u:r:su:s0
 on property:vendor.sys.usb.adb.disabled=*
     setprop sys.usb.adb.disabled ${vendor.sys.usb.adb.disabled}
 
-# Set default value on sys.usb.configfs early in boot sequence. It will be
-# overridden in `on boot` action of init.hardware.rc.
-on init
-    setprop sys.usb.configfs 0
-
 # Used to disable USB when switching states
 on property:sys.usb.config=none && property:sys.usb.configfs=0
     stop adbd

commit f4d67ed5346e2f1284ddcfc928af3d0718c9b341

Make udc_timeout atomic
...to ensure that we can cancel the SetUsbController() thread in
a timely manner. Without holding a mutex or using an atomic varible,
setting udc_timeout=True by the main init process may get lost in the
cache and not flushed out for the SetUsbController() thread within the
10s timeout window.

Test: verify UDC comes up in regular/recovery/fastbootd mode
Bug: 448889416
Fixes: 4397f3730be8 ("Refactor USB controller setup to be more robust")
Change-Id: I432acb9f89724f7dea306bb4dc9023f77680a045
Signed-off-by: Will McVicker <willmcvicker@google.com>

init/init.cpp
@@ -139,7 +139,7 @@ struct PendingControlMessage {
 [[clang::no_destroy]] static std::condition_variable udc_detection_cv;
 [[clang::no_destroy]] static std::mutex udc_controller_lock;
 static auto udc_controller_set = false;
-static auto udc_timeout = false;
+static std::atomic<bool> udc_timeout = false;
 
 // Init epolls various FDs to wait for various inputs.  It previously waited on property changes
 // with a blocking socket that contained the information related to the change, however, it was easy